5e8fed0fb290da8556bea12b322194ad4427e59a
Suho0314 <suholee0314@gmail.com>
2025-11-10 12:57:47 +0900

    [LSH] add aug_hub_agg.cleaned.csv file

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from dataset import create_dataloaders
 from utils import hist_conversations_length
 
 # 파일 경로 설정
-train_file_path = "./Data/aiffel-dl-thon-dktc-online-15/aug_hub_cleaned.csv"
+train_file_path = "./Data/aiffel-dl-thon-dktc-online-15/aug_hub_agg_cleaned.csv"
 test_file_path = "./Data/aiffel-dl-thon-dktc-online-15/test.csv"
 
 # max_length 확인하기
