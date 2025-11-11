8d89096eb7e1ecb8e4d89575e955f73058dd0535
Suho0314 <suholee0314@gmail.com>
2025-11-11 11:18:14 +0900

    [LSH] Last commit

---
+++ eda.py
@@ -0,0 +1,9 @@
+from dataset import create_dataloaders
+from utils import hist_conversations_length
+
+# 파일 경로 설정
+train_file_path = "./Data/aiffel-dl-thon-dktc-online-15/train_agg.csv"
+test_file_path = "./Data/aiffel-dl-thon-dktc-online-15/test.csv"
+
+# max_length 확인하기
+hist_conversations_length(train_file_path, test_file_path, vocab_size=1350)
--- train.py
+++
@@ -1,9 +0,0 @@
-from dataset import create_dataloaders
-from utils import hist_conversations_length
-
-# 파일 경로 설정
-train_file_path = "./Data/aiffel-dl-thon-dktc-online-15/aug_hub_agg_cleaned.csv"
-test_file_path = "./Data/aiffel-dl-thon-dktc-online-15/test.csv"
-
-# max_length 확인하기
-hist_conversations_length(train_file_path, test_file_path, vocab_size=1350)
